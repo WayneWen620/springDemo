eade3398952cc3f632b8af0ad823c14db1f8e494
stevenwen <steven.wen@tpisoftware.com>
2025-10-09 11:36:01 +0800

    [功能]整合前端視圖-thymeleaf

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    //    @RequestMapping("/list")
+//    public String list(Model model) {
+//        List<User> list = new ArrayList<User>();
+//        list.add(new User(1,"a11",18));
+//        list.add(new User(2,"b11",23));
+//        list.add(new User(3,"c11",30));
+//
+//        model.addAttribute("list",list);
+//        return "list";
+//    }
+    @RequestMapping("/demo1")
+    public String demo(Model model) {
+        model.addAttribute("message","您好,Thymeleaf");
+        return "demo1";
+    }
+
+    @RequestMapping("/demo2")
+    public String demo2(Model model) {
+        model.addAttribute("name","alan");
+        return "demo2";
+    }
+
+    @RequestMapping("/demo3")
+    public String demo3(Model model) {
+        model.addAttribute("gender","男");
+        model.addAttribute("grade",2);
+
+
+        return "demo2";
+    }
+}
