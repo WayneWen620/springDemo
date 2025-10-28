7f8dfdcb5341a5b6a095e084be9cf8c292e6250e
stevenwen <steven.wen@tpisoftware.com>
2025-10-28 09:58:49 +0800

    [權限]新增強制使用者使用強密碼方法&增加bcrypt加解密

--- src/main/java/com/example/demo/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/demo/config/ProjectSecurityConfig.java
@@ -2,13 +2,17 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,8 +33,24 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user=User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin=User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails user=User.withUsername("user").password("{noop}!QAZ@12345").authorities("read").build();
+        UserDetails admin=User.withUsername("admin").password("{bcrypt}$2a$10$0rAtBWAVJxIsHPifJJAiAuTitLTx9EW67Fj/65id/JEqBIbftGxOC").authorities("admin").build();
         return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     *
+     * 強制使用者使用強密碼
+     * 檢查使用者的密碼是否曾出現在資料外洩事件中（也就是「被洩漏過的密碼」
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
